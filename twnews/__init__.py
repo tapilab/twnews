708377f83d9962b21ca48f104e8c3e68d4822e3e
Aron Culotta <aronwc@gmail.com>
2014-04-09 15:40:20 -0500

    add data path to init

--- twnews/__init__.py
+++ twnews/__init__.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
+
 __author__ = 'Aron Culotta'
 __email__ = 'aronwc@gmail.com'
 __version__ = '0.1.0'
+
+__data__ = os.environ.get('TWNEWS_DATA')
