bc025b45062e25669b6ebc39271b8f43dff6439c
Aron Culotta <aronwc@gmail.com>
2014-04-09 15:40:09 -0500

    load balance scores from file

--- twnews/balance.py
+++ twnews/balance.py
@@ -15,6 +15,7 @@ sys.stdout = codecs.getwriter('utf8')(sys.stdout)
 
 
 def fetch_balance_scores(outf=__data__ + '/urls.tsv'):
+    """ Fetch json data and store as tab-separated file. """
     out = codecs.open(outf, "w", "utf-8")
     js = json.loads(requests.get(JSON_URL).text)
     for url in js:
@@ -22,5 +23,12 @@ def fetch_balance_scores(outf=__data__ + '/urls.tsv'):
     out.close()
 
 
+def load_balance_scores(fname=__data__ + '/urls.tsv'):
+    """ Read from tab-separated file to Python dict. """
+    inf = codecs.open(fname, "r", "utf-8")
+    return dict([(line.split()[0], float(line.split()[1])) for line in inf.readlines()])
+
+
 if __name__ == '__main__':
     fetch_balance_scores()
+    print load_balance_scores()
