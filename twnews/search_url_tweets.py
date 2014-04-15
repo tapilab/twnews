5e19e9d6282c7ca5059d96b14ec5c19264fe7959
Aron Culotta <aronwc@gmail.com>
2014-04-15 11:31:09 -0500

    put twitter query in separate thread to avoid infinite hang

--- twnews/search_url_tweets.py
+++ twnews/search_url_tweets.py
@@ -14,6 +14,9 @@ Note that this will introduce duplicates, which should be filtered downstream.
 """
 import codecs
 import json
+#from multiprocessing import Process, Queue
+from threading import Thread
+from Queue import Queue
 import time
 
 from twitter.api import TwitterHTTPError
@@ -23,23 +26,41 @@ from . import __data__
 from .balance import load_balance_scores
 
 
+def do_search(twapi, qu, url, score, count=100, result_type='recent'):
+    global results
+    domain = url[7:]
+    while True:
+        try:
+            result = twapi.search.tweets(q=domain, count=100, result_type='recent')
+            tweets = []
+            for tweet in result['statuses']:
+                tweet['url_query'] = url
+                tweet['url_score'] = score
+                tweets.append(tweet)
+            qu.put(tweets)
+            return
+        except TwitterHTTPError as e:
+            print 'Error: ', e.e.code
+            time.sleep(300)
+
+
 def search_for_tweets(scores, fname=__data__ + '/tweets.json'):
     out = codecs.open(fname, 'a', 'utf-8')
     twapi = twutil.api.twapi
+    qu = Queue()
     while True:
         for url, score in scores.iteritems():
-            try:
-                domain = url[7:]
-                result = twapi.search.tweets(q=domain, count=100, result_type='recent')
-                print 'found', len(result['statuses']), 'for', domain
-                for tweet in result['statuses']:
-                    tweet['url_query'] = url
-                    tweet['url_score'] = score
+            p = Thread(target=do_search, args=(twapi, qu, url, score))
+            p.start()
+            p.join(900)
+            if p.is_alive():
+                print 'no results after 15 minutes for', url, '. continuing.'
+            else:
+                results = qu.get()
+                print 'found', len(results), 'for domain', url
+                for tweet in results:
                     out.write(json.dumps(tweet) + '\n')
                 time.sleep(2)
-            except TwitterHTTPError as e:
-                print 'Error: ', e.e.code, '   Sleeping for 5 minutes...'
-                time.sleep(300)
 
 
 if __name__ == '__main__':
