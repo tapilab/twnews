65ae7efa88f3c5b2b6518ace53663ef075bc3090
Aron Culotta <aronwc@gmail.com>
2015-01-29 10:36:46 -0600

    uniqify tweet ids

--- twnews/print_urls_by_user.py
+++ twnews/print_urls_by_user.py
@@ -27,11 +27,14 @@ def print_urls_by_user(tweets_file=__data__ + '/tweets.json'):
     user2urls = defaultdict(lambda: set())
     url2score = defaultdict(lambda: 0.)
     inf = codecs.open(tweets_file, 'rt', 'utf-8')
+    ids = set()
     for line in inf:
         js = json.loads(line)
         if 'url_query' in js:  # valid line
-            user2urls[js['user']['screen_name']].add(js['url_query'])
-            url2score[js['url_query']] = float(js['url_score'])
+            if js['id'] not in ids:
+                user2urls[js['user']['screen_name']].add(js['url_query'])
+                url2score[js['url_query']] = float(js['url_score'])
+                ids.add(js['id'])
     for user, urls in user2urls.iteritems():
         if len(urls) > 1:
             dist = average_distance([url2score[u] for u in urls])
